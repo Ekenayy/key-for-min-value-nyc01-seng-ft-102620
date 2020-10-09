72002ca664f9e8df5cc4751239a1aee5e301c948
Ekene N <ekenayy@gmail.com>
2020-10-09 01:24:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
 if name_hash.empty?
  return nil
 else 
-  name_hash.each do
+  name_hash.each do |key, value|
  if lowest_value == 0 || value < lowest_value
       lowest_value = value 
       lowest_key = key
